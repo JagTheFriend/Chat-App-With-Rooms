34fb0bba8e28093f68fe5d856c2ee5c475453bd4
JagTheFriend <jagthefriend12@gmail.com>
2021-11-29 13:59:55 +0200

    Send the message to all the other clients

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,8 @@
 import { Server } from 'socket.io';
 export function handleSocketIo(io: Server) {
   io.on('connect', socket => {
-    socket.emit('chat-message', 'hello world');
+    socket.on('send-chat-message', (message: string) => {
+      socket.broadcast.emit('chat-message', message);
+    });
   });
 }
