2a4fc055a4afd5a005d4be5152adf33feabd88a0
JagTheFriend <jagthefriend12@gmail.com>
2021-11-29 14:26:41 +0200

    Created a simple chat app

--- src/public/script.js
+++ src/public/script.js
@@ -1,13 +1,33 @@
 const socket = io('http://localhost:3000');
 const messageForm = document.getElementById('send-container');
 const message = document.getElementById('message-input');
+const messageContainer = document.getElementById('message-container');
+const name = prompt('Please enter your name: ');
+appendMessage('You joined!');
+
+socket.emit('new-user', name);
 
 socket.on('chat-message', data => {
-  console.log(data);
+  appendMessage(`${data.name}: ${data.message}`);
+});
+
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected!`);
+});
+
+socket.on('user-disconnect', name => {
+  appendMessage(`${name} disconnected!`);
 });
 
 messageForm.addEventListener('submit', event => {
   event.preventDefault();
   socket.emit('send-chat-message', message.value);
+  appendMessage(`${name}: ${message.value}`);
   message.value = '';
 });
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+}
--- src/socket.ts
+++ src/socket.ts
@@ -1,8 +1,21 @@
 import { Server } from 'socket.io';
+
+const users = {};
 export function handleSocketIo(io: Server) {
   io.on('connect', socket => {
     socket.on('send-chat-message', (message: string) => {
-      socket.broadcast.emit('chat-message', message);
+      socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+    });
+
+    socket.on('new-user', (name: string) => {
+      users[socket.id] = name;
+      socket.broadcast.emit('user-connected', name);
+    });
+
+    socket.on('disconnect', () => {
+      const name: string = users[socket.id];
+      delete users[socket.id];
+      socket.broadcast.emit('user-disconnect', name);
     });
   });
 }
