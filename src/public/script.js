18b1c8597daa5b997b1901b408af9e9a808021c5
JagTheFriend <jagthefriend12@gmail.com>
2021-11-29 13:57:32 +0200

    The client could now send the message to the server

--- src/public/script.js
+++ src/public/script.js
@@ -1,4 +1,13 @@
 const socket = io('http://localhost:3000');
+const messageForm = document.getElementById('send-container');
+const message = document.getElementById('message-input');
+
 socket.on('chat-message', data => {
   console.log(data);
+});
+
+messageForm.addEventListener('submit', event => {
+  event.preventDefault();
+  socket.emit('send-chat-message', message.value);
+  message.value = '';
 });
