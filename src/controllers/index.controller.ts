850b5effdc85e19fd17ac71257fb1d60a0e08194
JagTheFriend <jagthefriend12@gmail.com>
2021-11-29 13:18:21 +0200

    Send the html file

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,7 +3,8 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.sendStatus(200);
+      // res.sendStatus(200);
+      res.render('index');
     } catch (error) {
       next(error);
     }
