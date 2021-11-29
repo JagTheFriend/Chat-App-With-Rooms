59f794322432f1facbf40eb6e897fee4d3eeee4f
JagTheFriend <jagthefriend12@gmail.com>
2021-11-29 18:19:21 +0200

    Created a `/chat` route

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,14 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 
-class IndexController {
+class ChatController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
       // res.sendStatus(200);
-      // res.render('index');
+      res.render('index');
     } catch (error) {
       next(error);
     }
   };
 }
 
-export default IndexController;
+export default ChatController;
---
+++ src/routes/chat.route.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import ChatController from '@controllers/chat.controller';
+import { Routes } from '@interfaces/routes.interface';
+
+class ChatRoute implements Routes {
+  public path = '/chat';
+  public router = Router();
+  public chatController = new ChatController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}`, this.chatController.index);
+  }
+}
+
+export default ChatRoute;
